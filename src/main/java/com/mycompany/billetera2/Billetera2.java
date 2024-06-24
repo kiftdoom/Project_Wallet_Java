03279cb5cbd2a1522bd2d9e1707a9cf83be41969
kiftdoom <159972365+kiftdoom@users.noreply.github.com>
2024-06-23 23:43:00 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/billetera2/Billetera2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.billetera2;
+
+/**
+ *
+ * @author Usert
+ */
+public class Billetera2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
